91dba38651ed5281248921cd4092c5eff23a47de
HemalathaVell <hemavel4440@gmail.com>
2024-10-09 14:42:07 +0530

    firstname

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		int a = 123654;
+		int o = 0;
+		int count = 0;
+		int sum = 0;
+		
+		while(a>0) {
+		int b = a/10;
+		int c =a%10;
+		o=c+(o*10);
+		a=b;
+		count++;
+		sum = sum +c;
+		}
+		System.out.println(o);
+		
+		}
+	}
+
+
